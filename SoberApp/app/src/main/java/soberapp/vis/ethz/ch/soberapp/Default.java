3158e3d84b43b1ec27faa1f2da01a799a80950d7
Jonas Passerini <passuf@gmail.com>
2014-11-08 16:19:46 +0000

    validate input from IntroActivity

---
+++ SoberApp/app/src/main/java/soberapp/vis/ethz/ch/soberapp/Default.java
@@ -0,0 +1,11 @@
+package soberapp.vis.ethz.ch.soberapp;
+
+
+
+public class Default {
+
+    public static final int MIN_NAME_LENGTH = 1;
+    public static final int MIN_WEIGHT = 1;
+    public static final int MIN_HEIGHT = 1;
+
+}

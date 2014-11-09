1be6045afa59fc757596fb8dc49ca91237e962b3
Jonas Passerini <passuf@gmail.com>
2014-11-09 03:33:10 +0000

    implemented drinking history

---
+++ SoberApp/app/src/main/java/soberapp/vis/ethz/ch/soberapp/ConsumeAdapter.java
@@ -0,0 +1,61 @@
+package soberapp.vis.ethz.ch.soberapp;
+
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.ocpsoft.prettytime.PrettyTime;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import soberapp.vis.ethz.ch.soberapp.data.Consume;
+
+public class ConsumeAdapter extends ArrayAdapter<Consume> {
+
+    private final Context context;
+    private final ArrayList<Consume> itemsArrayList;
+
+    public ConsumeAdapter(Context context, ArrayList<Consume> itemsArrayList) {
+
+        super(context, R.layout.history_row, itemsArrayList);
+
+        this.context = context;
+        this.itemsArrayList = itemsArrayList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // Create inflater
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // Get rowView from inflater
+        View rowView = inflater.inflate(R.layout.history_row, parent, false);
+
+        // Get the two text view from the rowView
+        TextView labelView = (TextView) rowView.findViewById(R.id.label);
+        TextView valueView = (TextView) rowView.findViewById(R.id.value);
+
+        // Set the text for textView
+        labelView.setText(itemsArrayList.get(position).getDrink().toString());
+        Date date = new Date(itemsArrayList.get(position).getTsp());
+        PrettyTime prettyTime = new PrettyTime(Locale.UK);
+        String dateString = prettyTime.format(date);
+        //String dateString = new SimpleDateFormat(Default.DATETIME_FORMAT).format(date);
+        valueView.setText(dateString);
+
+        valueView.setGravity(Gravity.CENTER_VERTICAL);
+        labelView.setGravity(Gravity.CENTER_VERTICAL);
+
+        return rowView;
+    }
+}
--- SoberApp/app/src/main/java/soberapp/vis/ethz/ch/soberapp/DrinksHistoryActivity.java
+++ SoberApp/app/src/main/java/soberapp/vis/ethz/ch/soberapp/DrinksHistoryActivity.java
@@ -1,59 +1,45 @@
 package soberapp.vis.ethz.ch.soberapp;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 import soberapp.vis.ethz.ch.soberapp.data.Consume;
-import soberapp.vis.ethz.ch.soberapp.data.Drink;
 
 
-public class DrinksHistoryActivity extends Activity {
+public class DrinksHistoryActivity extends ListActivity {
 
     private static final String LOG_TAG = "DrinksHistoryActivity";
 
     private AlcoholLevelCalculator alc = AlcoholLevelCalculator.getInstance();
-    private ListView drinksListView;
-    private List<Consume> drinksList;
-    private List<String> drinkNames;
-    private ArrayAdapter<String> drinksAdapter;
+    private ArrayList<Consume> drinksList;
+    private ArrayAdapter<Consume> drinksAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks_history);
+        setTitle(R.string.title_activity_drinks_history);
 
         Calendar c = Calendar.getInstance();
         c.add(Calendar.HOUR, Default.CONSUMES_HISTORY);
+        drinksList = (ArrayList<Consume>) alc.getConsumor().consumed(c.getTimeInMillis());
 
-        // TODO: get correct data
-        Timestamp ts = new Timestamp(c.getTimeInMillis());
-        //drinksList = alc.getConsumor().consumed(ts);
-        drinksList = new ArrayList<Consume>();
-
-        drinksListView = (ListView) findViewById(R.id.drinks_listView);
-        drinkNames = new ArrayList<String>();
-        for (Consume con : drinksList) {
-            drinkNames.add(con.toString());
-        }
-        drinksAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, drinkNames);
-        drinksListView.setAdapter(drinksAdapter);
+        drinksAdapter = new ConsumeAdapter(this, drinksList);
+        setListAdapter(drinksAdapter);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.drinks_history, menu);
+        //getMenuInflater().inflate(R.menu.drinks_history, menu);
         return true;
     }
 

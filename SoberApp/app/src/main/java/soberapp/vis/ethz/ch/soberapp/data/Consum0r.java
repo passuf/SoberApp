60e440d2a81ecdc7f7afaa338de9667c45214d63
Philipp Gamper <phgamper@phgamper.ch>
2014-11-09 01:50:18 +0100

    Consum0r fixed

--- SoberApp/app/src/main/java/soberapp/vis/ethz/ch/soberapp/data/Consum0r.java
+++ SoberApp/app/src/main/java/soberapp/vis/ethz/ch/soberapp/data/Consum0r.java
@@ -14,30 +14,26 @@ import static com.orm.SugarRecord.findWithQuery;
 
 public class Consum0r {
     public static final int TOP_N = 3;
-    private Consume last;
     private AlcoholLevelCalculator calc;
 
     public Consum0r(AlcoholLevelCalculator calc) {
         this.calc = calc;
-        List<Consume> list = find(Consume.class, null, null, null, "tsp DESC", "1");
-        last = !list.isEmpty() ? list.get(0) : null;
     }
 
     public void consume(Drink drink) {
-        Consume c = new Consume(drink, last, calc.getAlcoholLevel());
-        c.save();
-        last = c;
+      new Consume(drink, last(), calc.getAlcoholLevel()).save();
     }
 
     public void pukeLast() {
-        Consume c = last;
-        last = last.getLast();
-        c.delete();
+        Consume last = last();
+        if(last != null) {
+            last.delete();
+        }
     }
 
     public List<Drink> topN() {
         List<Drink> drinks = new LinkedList<Drink>();
-        Consume item = last;
+        Consume item = last();
         int i = 0;
         while (item != null && i < TOP_N) {
             if (!drinks.contains(item.getDrink())) {
@@ -63,18 +59,25 @@ public class Consum0r {
     }
 
     public List<Consume> consumed(Timestamp since) {
-        return findWithQuery(Consume.class, "SELECT * FROM Consume WHERE tsp > " + since.toString());
+        return findWithQuery(Consume.class, "SELECT * FROM Consume WHERE tsp > '" + since.toString() + "'");
     }
 
     public List<Consume> consumed(Timestamp since, Timestamp until) {
-        return findWithQuery(Consume.class, "SELECT * FROM Consume WHERE tsp BETWEEN " + since.toString() + " and " + since.toString());
+        return findWithQuery(Consume.class, "SELECT * FROM Consume WHERE tsp BETWEEN '" + since.toString() + "' and '" + until.toString() +"'");
+    }
+
+    public Consume last(){
+        List<Consume> list = find(Consume.class, null, null, null, "tsp DESC", "1");
+        return !list.isEmpty() ? list.get(0) : null;
     }
 
     public double level() {
+        Consume last = last();
         return last != null ? last.getLevel() : 0;
     }
 
     public Timestamp time() {
+        Consume last = last();
         return last != null ? last.getTsp() : new Timestamp(0);
     }
 }

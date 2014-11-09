12d6daaf656cfbe76b80e00d287193c2c60b6fab
Philipp Gamper <phgamper@phgamper.ch>
2014-11-09 02:57:22 +0100

    Database fixed

--- SoberApp/app/src/main/java/soberapp/vis/ethz/ch/soberapp/data/Consum0r.java
+++ SoberApp/app/src/main/java/soberapp/vis/ethz/ch/soberapp/data/Consum0r.java
@@ -58,12 +58,12 @@ public class Consum0r {
         return find(Drink.class, null, null, null, "name ASC", null);
     }
 
-    public List<Consume> consumed(Timestamp since) {
-        return findWithQuery(Consume.class, "SELECT * FROM Consume WHERE tsp > " + since.toString());
+    public List<Consume> consumed(long since) {
+        return findWithQuery(Consume.class, "SELECT * FROM Consume WHERE tsp > " + since);
     }
 
-    public List<Consume> consumed(Timestamp since, Timestamp until) {
-        return findWithQuery(Consume.class, "SELECT * FROM Consume WHERE tsp BETWEEN "+ since.toString() + " and " + until.toString());
+    public List<Consume> consumed(long since, long until) {
+        return findWithQuery(Consume.class, "SELECT * FROM Consume WHERE tsp BETWEEN "+ since + " and " + until);
     }
 
     public Consume last(){

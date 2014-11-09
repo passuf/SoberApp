eb09fa19b6c3e86008a38797e373ee23bdaa81ce
Jonas Passerini <passuf@gmail.com>
2014-11-09 10:39:01 +0000

    changed initial data

--- SoberApp/app/src/main/java/soberapp/vis/ethz/ch/soberapp/data/InitialData.java
+++ SoberApp/app/src/main/java/soberapp/vis/ethz/ch/soberapp/data/InitialData.java
@@ -4,18 +4,19 @@ import static com.orm.SugarRecord.listAll;
 public class InitialData {
     public static void initDB(){
         if(listAll(Drink.class).size() == 0) {
-            new Drink("Stange", 5.0, 300).save();
-            new Drink("Chübel", 5.0, 500).save();
-            new Drink("Mass", 5.0, 1000).save();
+            new Drink("Beer", 5.0, 300).save();
+            new Drink("Wine", 13, 100).save();
+            new Drink("Liquor", 17.0, 60).save();
+            new Drink("Beer", 5.0, 500).save();
+            new Drink("Beer", 5.0, 1000).save();
             new Drink("Stiefel", 5.0, 2000).save();
             new Drink("Cider", 4.0, 500).save();
             new Drink("Champagne", 12, 100).save();
-            new Drink("Wine", 13, 100).save();
             new Drink("Wine", 13, 200).save();
             new Drink("Shot", 40.0, 40).save();
             new Drink("Alcopops", 3.6, 330).save();
-            new Drink("Liquer", 17.0, 60).save();
             new Drink("Cocktail", 8.0, 300).save();
+            new Drink("Mass", 5.0, 1000).save();
             new Drink("Long Island Ice Tea", 13.0, 300).save();
         }
     }
